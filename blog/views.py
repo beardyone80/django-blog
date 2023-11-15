2a47a50394de5485a0a7543b043ae28e7cee3f09
Paul Beardwell <beardwell@hotmail.com>
2023-11-15 16:15:12 +0000

    filtered posts to only show status = published

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
