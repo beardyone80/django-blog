1b764b4cde6b2da222ce96682128148f7caf9dd0
Paul Beardwell <beardwell@hotmail.com>
2023-11-15 12:32:30 +0000

    registered comments and added some entries

--- blog/models.py
+++ blog/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
