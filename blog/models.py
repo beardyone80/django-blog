0ca7c50eb9d5d15fe6ff0422c5128754d544acd2
Paul Beardwell <beardwell@hotmail.com>
2023-11-15 14:04:56 +0000

    added meta data to models

--- blog/models.py
+++ blog/models.py
@@ -15,9 +15,21 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"The title of this post is {self.title} | written by {self.author}"
+
+    class Meta:
+        ordering = ["-created_on"]
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
