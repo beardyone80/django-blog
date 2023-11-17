1f6ac3934ba358dfd988e389fe91fbcb884174c0
Paul Beardwell <beardwell@hotmail.com>
2023-11-17 11:31:02 +0000

    added post detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-beardyone80-djangoblog-dapdi86qo93.ws-eu106.gitpod.io',
---
+++ staticfiles/js/script.js
@@ -0,0 +1,1 @@
+console.log("My static files are being collected")
