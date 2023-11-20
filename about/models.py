86456e926c9f2fd171b86b386f5709ce187bf48d
Paul Beardwell <beardwell@hotmail.com>
2023-11-20 16:00:30 +0000

    enabled adding of images to posts and about pages

--- about/models.py
+++ about/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
 
