41b8b2324030f71bfb3f9433c24f7faa0297182c
Paul Beardwell <beardwell@hotmail.com>
2023-11-20 11:32:54 +0000

    added collab form and POST handling

--- blog/views.py
+++ blog/views.py
@@ -32,6 +32,7 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Recieved a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -45,6 +46,7 @@ def post_detail(request, slug):
 
     comment_form = CommentForm()
 
+    print("Rendering template")
     return render(
         request,
         "blog/post_detail.html",
