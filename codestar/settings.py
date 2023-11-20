51f97aee7c73ea622f492bdc0f7b89134d0b86c5
Paul Beardwell <beardwell@hotmail.com>
2023-11-20 15:07:42 +0000

    added cloudinary to serve image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
+    'blog',
     'about',
 ]
 
